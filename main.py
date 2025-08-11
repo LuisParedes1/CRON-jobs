df9c1aab19e62b9abfd2eb5e1dcb1007a8504bbb
Luis Paredes <luisjosepr@gmail.com>
2025-08-11 09:41:12 -0300

    added logger

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
 from datetime import datetime
+import logging
 
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    logger.info(f"Starting at {datetime.now()}")
 
     data = {
         "name": "sample",
@@ -11,3 +16,5 @@ if __name__ == "__main__":
 
     with open("sample.json", "w") as f:
         json.dump(data,f)
+
+    logger.info(f"Finished at {datetime.now()}")
